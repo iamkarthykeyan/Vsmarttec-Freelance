22fa97bc195c1844f4e6885be32281cebb8df33f
Karthikeyan <karthikeyan02092003@gmail.com>
2024-09-14 20:17:11 +0530

    Added JobOpenings Component

--- vsmt/src/App.jsx
+++ vsmt/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './Components/Navbar/Navbar'
 import { Routes,Route } from 'react-router-dom'
 import HomePage from './Components/Pages/HomePage'
 import About from './Components/Pages/About'
+import JobOpenings from './Components/Pages/JobOpenings'
 function App() {
 
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage></HomePage>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/jobopenings' element={<JobOpenings/>}></Route>
         </Routes>
         <Footer></Footer>
     
